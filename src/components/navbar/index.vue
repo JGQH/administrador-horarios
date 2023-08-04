875c87fbd52c8bfeb553bd8e9d122a462d775083
JGQH <>
2023-08-04 00:19:14 -0500

    Crear estructura base de las páginas

---
+++ src/components/navbar/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav>
+        <h1>Creador de horarios</h1>
+        <div v-for="ruta in rutas">
+            <a :href="ruta === 'Generar' ? '/' : ruta.toLowerCase()">{{ ruta }}</a>
+        </div>
+    </nav>
+</template>
+
+<script setup lang="ts">
+    const rutas: string[] = ["Generar", "Registrar"]
+</script>
